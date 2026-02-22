6ff0a0560a342b49ae26a80d5eb73f0a92473ca1
Pasubu.book <163115290+pasububook@users.noreply.github.com>
2026-02-22 11:39:27 +0000

    オフライン対応を修正

--- src/script/script.js
+++ src/script/script.js
@@ -6,7 +6,7 @@ import './main.js';
 
 if ('serviceWorker' in navigator) {
 	window.addEventListener('load', () => {
-		navigator.serviceWorker.register('./script/sw.js').catch((error) => {
+		navigator.serviceWorker.register('./sw.js').catch((error) => {
 			console.error('Service Worker registration failed:', error);
 		});
 	});
